64dbad914426aa3489cf9436518db82ad19cc4e0
Miguel Angel <vivanlasfallas@hotmail.com>
2021-05-27 15:08:39 +0200

    varios cambios
    Que me voy a trabajar!

---
+++ ejerciciosJs/ejercicios.js
@@ -0,0 +1,17 @@
+function ejercicioIf() {
+    let edad, texto;
+
+    edad = prompt("Edad? ");
+    if (!isNaN(edad) && edad > 0) {
+
+        texto = edad < 18 ? "<h3> Eres menor</h3>" : "<h3> Eres mayor de edad</h3>";
+
+    } else if (isNaN(edad)) {
+        texto = `<h3>--${edad.toLocaleUpperCase()}-- no es un número</h3>`;
+    } else if (edad < 0) {
+
+        texto = `<h3>//${edad}// Es un número negativo</h3>`;
+    }
+
+    return document.write(texto);
+}
