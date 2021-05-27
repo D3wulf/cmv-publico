d3a867f9124d29d79504ae2258a0b78171104941
Miguel Angel <vivanlasfallas@hotmail.com>
2021-05-27 13:49:26 +0200

    Lo que se hizo el dia 27
    Algunas prácticas de Javascript

---
+++ assets/js/27-05-2021.js
@@ -0,0 +1,37 @@
+// let edad, texto;
+
+// edad = prompt("Edad? ");
+// if (!isNaN(edad) && edad > 0) {
+
+//     texto = edad < 18 ? "<h3> Eres menor</h3>" : "<h3> Eres mayor de edad</h3>";
+
+// } else if (isNaN(edad)) {
+//     texto = `<h3>--${edad.toLocaleUpperCase()}-- no es un numero</h3>`;
+// } else if (edad < 0) {
+
+//     texto = `<h3>//${edad}// Es un número negativo</h3>`;
+// }
+
+// document.write(texto);
+
+
+
+// let num1 = 0;
+// let num2 = 0;
+
+// dato = prompt("Introduzca el primero número ");
+// num1 =
+
+
+
+// EJERCICIO DOM URL //
+
+function miFuncion() {
+    let x = "El archivo con el que estas trabajando es: " + document.baseURI;
+
+    let y = "Altura disponible: " + screen.availHeight + "px";
+
+    let z = "Ancho disponible: " + screen.availWidth + "px";
+
+    document.getElementById("demo").innerHTML = x + "<br>" + y + "<br>" + z;
+}
