e25a83c1775307e23e4e94b20dee6fe21fcddb37
Miguel Angel <vivanlasfallas@hotmail.com>
2021-06-03 11:48:28 +0200

    añadida busqueda
    Se añade la búsqueda que dará la url como resultado

--- prototypes/31-05ConImg/31-05ConImg.js
+++ prototypes/31-05ConImg/31-05ConImg.js
@@ -18,6 +18,7 @@ function cargarArray(xml) {
     let Nom;
     let Foto;
     let Pie;
+    //let Detalle;
     let coche = [];
     let xmlDoc = xml.responseXML;
     let tabla = "";
@@ -30,19 +31,16 @@ function cargarArray(xml) {
         Nom = x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue;
         Foto = x[i].getElementsByTagName("foto")[0].childNodes[0].nodeValue;
         Pie = x[i].getElementsByTagName("pie")[0].childNodes[0].nodeValue;
-        // actualizo la tabla de visualización
-        tabla += "<h1>" + Nom + "</h1><br>"
-        tabla += Foto + "<br>" + Pie + " <br>";
-        // actualizo el array bidimensional con los usuarios coches
+        //Detalle = x[i].getElementsByTagName("detalle")[0].childNodes[0].nodeValue;
+
         coche = [Nom, Foto, Pie];
         coches.push(coche);
 
     }
 
+    console.log(coches.length);
+
     tabla += ""
-        //document.getElementById("contenidoXML").innerHTML = tabla;
-
-
 }
 
 function mostrar() {
@@ -51,33 +49,60 @@ function mostrar() {
 
     let tabla = "";
 
-    coches.forEach(
-        (coche) => {
-            coche.forEach(
-                (datos) => {
 
 
+    //tabla += "<div >";
 
-                    //tabla += "<div >";
-
-                    for (let i = 0; i < coches.length; i++) {
-                        // leo las etiquetas que me interesan del objeto
-                        Nom = coches[i][0];
-                        Foto = coches[i][1];
-                        Pie = coches[i][2];
-                        // actualizo la tabla de visualización
-                        tabla += `<h3>${Nom}</h3>`;
-                        tabla += `<img src="${Foto}" class='img-fluid' alt="">`;
-                        tabla += `<p class=''>${Pie}</p>`;
-                        //tabla += "<a href='#' class='btn btn-primary'>Ir</a>"
+    for (let i = 0; i < coches.length; i++) {
+        // leo las etiquetas que me interesan del objeto
+        Nom = coches[i][0];
+        Foto = coches[i][1];
+        Pie = coches[i][2];
+        // actualizo la tabla de visualización
+        tabla += `<h3>${Nom}</h3>`;
+        tabla += `<img src="${Foto}" class='img-fluid' alt="">`;
+        tabla += `<p class=''>${Pie}</p>`;
+        //tabla += "<a href='#' class='btn btn-primary'>Ir</a>"
 
 
-                        // actualizo el array bidimensional con los usuarios coches
+        // actualizo el array bidimensional con los usuarios coches
 
-                    }
+    }
 
-                    //tabla += "</div>";
-                });
-        });
+    //tabla += "</div>";
+
+
     document.getElementById("contenidoXML").innerHTML = tabla;
+}
+
+let miCoche;
+
+function mostrarUrl() {
+
+    miCoche = document.getElementById("coche").value;
+
+    console.log(miCoche);
+
+    let encontrado = 0;
+    let url = "";
+
+    for (let i = 0; i < coches.length; i++) {
+
+        if (miCoche == coches[i][0]) {
+
+            encontrado = 1;
+            url = coches[i][1];
+        }
+    }
+
+    if (encontrado == 1) {
+
+        console.log("Se han encontrado resultados");
+        console.log(url);
+    } else {
+        alert("No se han encontrado resultados a su búsqueda");
+    }
+
+    document.getElementById("contenido4").innerHTML = url;
+
 }
